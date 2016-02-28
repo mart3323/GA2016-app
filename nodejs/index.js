88997683197d2d5e2b78434845573ea4b76783c8
mart3323 <mart3323@gmail.com>
2016-02-28 11:08:07 +0200

    Convert to promises, much easier to wrap head around

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,39 @@
-var socketServer = require("./js/socketServer.js");
-var Server = new socketServer(7332, function(client, type, msg){
-    if(type == "register"){
-        console.log("A user registered → "+msg);
-        client.emit("registration approved", {});
-    } else {
-        console.log("An unknown message was received → "+type+" → "+msg);
-    }
-});
-Server.listenFor(["msg", "pm", "register"]);
-console.log("Server started");
+var ApplicationServer = require("./js/Protocol layer/ServerProtocol.js");
 
+//var Server = new ApplicationServer(7332,[]);
+var io_module = require("socket.io");
+var Promise = require("promise");
 
+var io = io_module.listen(7332);
+
+io.on('connection', function(socket){
+    console.log("→ Connect");
+    challenge_and_verify(socket)
+        .then(serve)
+}.bind(this));
+
+function challenge_and_verify(socket){
+    return new Promise(function(resolve, reject){
+        console.log("← Challenge");
+        socket.emit("Challenge", "test");
+        socket.on("Register", function(){
+            console.log("→ Register");
+            console.log("← Registered");
+            socket.emit("Registered", true);
+            resolve();
+        });
+        reject_on_disconnect(socket, reject);
+    });
+}
+function serve(socket){
+    return new Promise(function(resolve, reject){
+        reject_on_disconnect(socket, reject);
+    });
+}
+
+function reject_on_disconnect(socket, reject){
+    socket.on("disconnect", function(){
+        console.log("→ Disconnect");
+        reject();
+    })
+}
